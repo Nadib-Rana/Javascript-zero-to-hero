6064200ff5c11b3799d3936b04c259d41a878c92
Nadib Rana <codecrafersnadib@gmail.com>
2024-08-22 10:06:48 +0600

    Basic Syntax

---
+++ Basic JavaScript Syntax(1.2).js
@@ -0,0 +1,13 @@
+<!-- JavaScript syntax includes statements, variables, functions, and events. Here’s an example: -->
+// Single-line comment
+/* Multi-line comment */
+
+// Declare a variable
+let message = "Hello, World!";
+
+// Function that displays a message
+function showMessage() {
+    alert(message);
+}
+
+
