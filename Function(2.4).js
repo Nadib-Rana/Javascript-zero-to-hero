3b105c64c73b58e5f6d1e21e2d0e90edcb7a5afd
Nadib Rana <codecrafersnadib@gmail.com>
2024-08-25 09:50:59 +0600

    Parameters

---
+++ Function(2.4).js
@@ -0,0 +1,7 @@
+// Definition: Parameters are placeholders for the values that a function expects when it's called. They are defined in the function declaration.
+// Location: Parameters are written inside the parentheses of the function definition.
+
+Example:
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
