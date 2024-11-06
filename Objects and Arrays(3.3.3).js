d31c620d878309ac179645e2f9a0de78aac64e83
Nadib Rana <codecrafersnadib@gmail.com>
2024-11-06 20:34:09 +0600

    Delete Objects and Arrays(3.3.3).js

--- Objects and Arrays(3.3.3).js
+++
@@ -1,8 +0,0 @@
-// Function Parameter Destructuring:
-
-function printPerson({ name, age }) {
-  console.log(`Name: ${name}, Age: ${age}`);
-}
-
-const person = { name: "Bob", age: 30 };
-printPerson(person);  // Name: Bob, Age: 30
