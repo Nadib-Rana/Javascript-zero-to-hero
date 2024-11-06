a3ef900c63be519ecb25b7e7ce7d7117fe157294
nadib <nadibrana9@gmail.com>
2024-11-06 20:09:59 +0600

    File sorting

---
+++ Lesson 1/Basic JavaScript Syntax(1.2).js
@@ -0,0 +1,15 @@
+ // JavaScript syntax includes statements, variables, functions, and events. Here’s an example: 
+
+// Single-line comment
+/* Multi-line comment */
+
+// Declare a variable
+let message = "Hello, World!";
+
+// Function that displays a message
+function showMessage() {
+    alert(message);
+}
+
+
+
---
+++ Lesson 1/Basics of JavaScript(1.3).js
@@ -0,0 +1,57 @@
+                             // Data type
+////////////////1. Primitive Data Types:
+
+ // 1.1 String:
+  //  Examples-
+       let name = "John";  // Double quotes
+       let greeting = 'Hello!';  // Single quotes
+       let template = `Hello, ${name}!`;  // Backticks for template literals
+ 
+
+// 1.2 Number:
+       let age = 25;
+       let price = 99.99;
+
+// 1.3 Bolean:
+        let isOnline = true;
+        let isAdmin = false;
+ 
+// 1.4 Undefined:
+       let user;  // Undefined by default
+
+// 1.5 Null: 
+     let empty = null;
+ 
+// 1.6 Symbol:
+      let uniqueId = Symbol("id");
+ 
+ // 1.7 BigInt: 
+      let bigNumber = BigInt(1234567890123456789012345678901234567890n);
+
+
+///////////////2. Non-Primitive Data Types (Objects)
+
+    //2.1 Object:
+        let person = {
+        name: "John",
+       age: 30,
+       isStudent: false
+      };
+   
+//2.2 Array:
+       let fruits = ["Apple", "Banana", "Cherry"];
+ 
+//2.3 Function: 
+      function greet() {
+    console.log("Hello!");
+    }
+  
+  
+ 
+
+
+
+
+       
+
+
